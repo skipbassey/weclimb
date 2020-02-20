23bf354b2f18a9c292bf4c0c29523cc4b47feaa2
okwongbassey <skip.bassey@duke-energy.com>
2020-02-20 08:54:36 -0500

    added blank functions for getting/setting schedule

--- src/services/appointment.service.ts
+++ src/services/appointment.service.ts
@@ -1,6 +1,44 @@
 import { Injectable } from "@angular/core";
+import { Observable } from 'rxjs';
+import { Appointment } from 'src/models/Appointment';
+import { HttpClient } from '@angular/common/http';
 
 Injectable()
 export class AppointmentService {
+
+    constructor(http: HttpClient) { }
+
+    appointments: Appointment[] = [
+
+    ]
+
+    getLicensedLevelFirstSession(): Observable<any> {
+        return null;
+    }
+
+    getLicensedLevelInsurance(): Observable<any> {
+        return null;
+    }
+
+    getLicensedLevelSelfPay(): Observable<any> {
+        return null;
+    }
+
+    getMastersLevelIntake(): Observable<any> {
+        return null;
+    }
+
+    getMastersLevelSelfPay(): Observable<any> {
+        return null;
+    }
+
+    getAdolescentGroupSelfPay(): Observable<any> {
+        return null;
+    }
+
+    setLicensedLevelFirstSession(appointment: Appointment): void {
+        
+    }
+
     
 }
