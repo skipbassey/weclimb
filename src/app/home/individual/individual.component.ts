93fb55eb8c02bb809924e55f63a56aeea389ee1b
okwongbassey <skip.bassey@duke-energy.com>
2020-03-03 14:05:03 -0500

    styling updates

--- src/app/home/individual/individual.component.ts
+++ src/app/home/individual/individual.component.ts
@@ -5,7 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './individual.component.html',
   styleUrls: [
     './individual.component.scss',
-    '../../../theme/font.scss'],
+    '../../../theme/font.scss',
+    '../../../theme/button.scss'
+  ],
 })
 export class IndividualComponent implements OnInit {
 
